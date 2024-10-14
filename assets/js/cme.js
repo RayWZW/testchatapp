1a5d6ca0f6892d559ad687dc6f9558781327322a
RayWZW <harrysmith690089@gmail.com>
2024-10-14 12:21:23 -0600

    fix

--- assets/js/cme.js
+++ assets/js/cme.js
@@ -365,7 +365,28 @@ return ''; // Return empty string if the URL doesn't match
 }
 
 
+function formatLinks(message) {
+const urlPattern = /(?<!\S)(https?:\/\/[^\s]+)/g;
 
+return message.replace(urlPattern, function(url) {
+    const embedFunctions = [
+        embedYouTubeVideo,
+        embedYouTubeShorts,
+        embedDiscordInvite,
+        embedVimeo
+    ];
+
+    for (let embedFunction of embedFunctions) {
+        const embed = embedFunction(url);
+        if (embed) {
+            return embed; // Return the first embed that matches
+        }
+    }
+
+    // If no embeds were matched, return the original URL wrapped in an anchor tag
+    return `<a href="${url}" class="link" target="_blank" rel="noopener noreferrer">${url}</a>`;
+});
+}
 
 
 function formatCodeBlocks(message) {
